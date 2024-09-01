38d8fced0f20bdf561fc47aaada15fa0f06fc2a1
Henrique Ataide <139990659+henilveira@users.noreply.github.com>
2024-09-01 11:16:13 -0300

    Time: 58 ms (38.69%), Space: 16.4 MB (84.11%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -2,24 +2,8 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         string = str(x)
         
-        if len(string) == 2:
-            return string[0] == string[1]
-                
-        
         if string == string[::-1]:
             return True
         else:
             return False
         
-#         array = list(string)
-        
-#         array = array[1:-1]
-        
-#         arr = [int(c) for c in array]
-        
-#         res_xor = 0
-        
-#         for num in arr:
-#             res_xor ^= num
-        
-#         return res_xor == 0
